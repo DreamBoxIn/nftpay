e218723930b7d2d32d6e873691de45b1457b1590
BenjaDreamBox <b.lattanzimerino@gmail.com>
2024-07-23 19:01:04 -0400

    Fix TypeScript type issue

--- src/app/actions/gate-condition.ts
+++ src/app/actions/gate-condition.ts
@@ -2,44 +2,18 @@ import { getContract } from "thirdweb";
 import { sepolia } from "thirdweb/chains";
 import { client } from "../consts/client";
 import { balanceOf as balanceOfERC721 } from "thirdweb/extensions/erc721";
-//import { balanceOf as balanceOfERC20 } from "thirdweb/extensions/erc20";
+import { Address } from "thirdweb"; // Asegúrate de importar el tipo Address correctamente
 
-export async function hasAccess(address: string): Promise<boolean> {
+export async function hasAccess(address: Address): Promise<boolean> {
   return await example_hasSomeErc721Tokens(address);
-  // return await example_hasSomeErc20Tokens(address);
 }
 
-//
-//
-//
-//
-//
-//
-//
-//
-/**
- * Check out some of the examples below
- * The use cases are not limited to token-balance, you can basically do anything you want.
- *
- * For example: You can leverage some third-party api to check for the "age" of the wallet and
- * only allow wallet older than 2 years to access.
- *
- * Or you can allow only wallets that have interacted with Uniswap to access the page!
- *
- * The sky is the limit.
- */
-
-async function example_hasSomeErc721Tokens(address: string) {
+async function example_hasSomeErc721Tokens(address: Address) {
   const requiredQuantity = 1n;
 
   const erc721Contract = getContract({
-    // replace with your own NFT contract address
     address: "0x3a9Dba1fC8602d924fb1b623EC391fc68840f731",
-
-    // replace with the chain that your nft contract was deployed on
-    // if that chain isn't included in our default list, use `defineChain`
     chain: sepolia,
-
     client,
   });
 
